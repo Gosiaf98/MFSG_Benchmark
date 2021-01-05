bf1b575dc5f9e0e0c2832d1296e52d52e2f5bd88
inf132221 <malgorzata.frackowiak@student.put.poznan.pl>
2021-01-05 13:35:01 +0100

    base template

--- MFSG/MFSG/settings.py
+++ MFSG/MFSG/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'MFSG.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "Benchmark/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
