cbb0d21fdb1214076a7e5a90663ef6efc7657246
inf132221 <malgorzata.frackowiak@student.put.poznan.pl>
2021-01-11 19:35:18 +0100

    measures validation done

--- MFSG/Benchmark/views/site/create.py
+++ MFSG/Benchmark/views/site/create.py
@@ -1,10 +1,10 @@
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic.edit import FormView
 from .form import Form
 from ...models import Site
 
 
-class Create(CreateView):
+class Create(FormView):
     template_name = "site_create.html"
     form_class = Form
     model = Site
--- MFSG/Benchmark/views/site/form.py
+++ MFSG/Benchmark/views/site/form.py
@@ -1,8 +1,16 @@
 from django import forms
+from django.contrib.messages.views import SuccessMessageMixin
+from django.utils.translation import ugettext_lazy as _
+
 from ...models import Site
 
 
-class Form(forms.ModelForm):
+class Form(SuccessMessageMixin, forms.ModelForm):
+
+    error_messages = {
+        'path_incorrect': _("Strona o podanym adresie nie istnieje"),
+        'no_measure': _("Musisz wybrać przynajmniej jedną miarę!")
+    }
 
     path = forms.CharField(label="Adres strony")
     dom = forms.BooleanField(required=False)
@@ -11,18 +19,21 @@ class Form(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(Form, self).__init__(*args, **kwargs)
 
+    def clean(self):
+        cleaned_data = super(Form, self).clean()
+
+        dom = cleaned_data.get('dom')
+        first_byte = cleaned_data.get('first_byte')
+        if not dom and not first_byte:
+            raise forms.ValidationError(
+                self.error_messages['no_measure'],
+                code='no_measure'
+            )
+
+        return cleaned_data
+
+        #TODO sprawdzić, czy istnieje strona o takim adresie
+
     class Meta:
         model = Site
         fields = ["path", "dom", "first_byte"]
-
-    def full_clean(self):
-        cleaned_data = super(Form, self).clean()
-
-        dom = self.cleaned_data.get('dom')
-        first_byte = self.cleaned_data.get('first_byte')
-
-        print("???")
-
-        return self.cleaned_data
-
-        #TODO sprawdzić, czy istnieje strona o takim adresie
