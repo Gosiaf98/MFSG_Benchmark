2d45830b5fdf712cb15a26835aa0a0d2a132fc18
inf132221 <malgorzata.frackowiak@student.put.poznan.pl>
2021-01-12 14:19:27 +0100

    save Site to db

--- MFSG/Benchmark/views/site/create.py
+++ MFSG/Benchmark/views/site/create.py
@@ -1,10 +1,12 @@
 from django.urls import reverse
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
 from .form import Form
 from ...models import Site
 
 
-class Create(FormView):
+class Create(CreateView):
     template_name = "site_create.html"
     form_class = Form
     model = Site
+    success_url = "/"
+
--- MFSG/Benchmark/views/site/form.py
+++ MFSG/Benchmark/views/site/form.py
@@ -1,23 +1,51 @@
 from django import forms
 from django.contrib.messages.views import SuccessMessageMixin
 from django.utils.translation import ugettext_lazy as _
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 from ...models import Site
 
 
 class Form(SuccessMessageMixin, forms.ModelForm):
 
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+
     error_messages = {
         'path_incorrect': _("Strona o podanym adresie nie istnieje"),
         'no_measure': _("Musisz wybrać przynajmniej jedną miarę!")
     }
 
-    path = forms.CharField(label="Adres strony")
+    path = forms.CharField(label="<b>Adres strony</b>")
     dom = forms.BooleanField(required=False)
     first_byte = forms.BooleanField(required=False)
 
+    SITES_TO_COMPARE = (
+        ("1", "One"),
+        ("2", "Two"),
+        ("3", "Three"),
+        ("4", "Four"),
+        ("5", "Five"),
+    )
+    compare_sites = forms.MultipleChoiceField(widget=forms.SelectMultiple, choices=SITES_TO_COMPARE, label="<b>Wybierz strony, z którymi chcesz się porównać:</b>", required=False)
+
     def __init__(self, *args, **kwargs):
         super(Form, self).__init__(*args, **kwargs)
+
+    def time_url(self, driver, url):
+        driver.get(url)
+
+        navigation_start = driver.execute_script(
+            "return window.performance.timing.navigationStart")
+        dom_complete = driver.execute_script(
+            "return window.performance.timing.domComplete")
+        first_byte = driver.execute_script(
+            "return window.performance.timing.responseStart")
+        total_time = dom_complete - navigation_start
+        first_byte_time = first_byte - navigation_start
+
+        return total_time, first_byte_time
 
     def clean(self):
         cleaned_data = super(Form, self).clean()
@@ -33,6 +61,27 @@ class Form(SuccessMessageMixin, forms.ModelForm):
         return cleaned_data
 
         #TODO sprawdzić, czy istnieje strona o takim adresie
+
+    def save(self, commit=True):
+        print("save")
+        try:
+            print(self.cleaned_data['path'])
+            total_time, first_byte_time = self.time_url(self.driver, self.cleaned_data['path'])
+            print(total_time)
+            print(first_byte_time)
+
+        finally:
+            site = Site()
+            site.title = self.driver.title
+            site.path = self.cleaned_data['path']
+            site.dom = total_time
+            site.first_byte = first_byte_time
+            site.save()
+            self.driver.close()
+            return site
+
+        return -1
+
 
     class Meta:
         model = Site
